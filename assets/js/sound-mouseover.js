3db266d629e9325482149805f779c17b764ccb4e
HenryNavarro <blackdemon.mx@hotmail.com>
2016-05-04 11:08:02 -0500

    mas sonidos

--- assets/js/sound-mouseover.js
+++ assets/js/sound-mouseover.js
@@ -106,3 +106,147 @@ function Duck()
 			}
 		}	
 }
+
+function Cow() 
+{
+	if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1))
+	{
+		if (document.all)
+		 {
+		  document.all.sound.src = "sounds/ingles/animales/cow.mp3";
+		 }
+	}
+
+	else {
+			{
+			var audio = document.getElementById("cow"); audio.play(); 
+			audio.play();
+			}
+		}	
+}
+
+function Chicken() 
+{
+	if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1))
+	{
+		if (document.all)
+		 {
+		  document.all.sound.src = "sounds/ingles/animales/chicken.mp3";
+		 }
+	}
+
+	else {
+			{
+			var audio = document.getElementById("chicken"); audio.play(); 
+			audio.play();
+			}
+		}	
+}
+
+function Frog() 
+{
+	if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1))
+	{
+		if (document.all)
+		 {
+		  document.all.sound.src = "sounds/ingles/animales/frog.mp3";
+		 }
+	}
+
+	else {
+			{
+			var audio = document.getElementById("frog"); audio.play(); 
+			audio.play();
+			}
+		}	
+}
+
+function Fish() 
+{
+	if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1))
+	{
+		if (document.all)
+		 {
+		  document.all.sound.src = "sounds/ingles/animales/fish.mp3";
+		 }
+	}
+
+	else {
+			{
+			var audio = document.getElementById("fish"); audio.play(); 
+			audio.play();
+			}
+		}	
+}
+
+function Mouse() 
+{
+	if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1))
+	{
+		if (document.all)
+		 {
+		  document.all.sound.src = "sounds/ingles/animales/mouse.mp3";
+		 }
+	}
+
+	else {
+			{
+			var audio = document.getElementById("mouse"); audio.play(); 
+			audio.play();
+			}
+		}	
+}
+
+function Bird() 
+{
+	if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1))
+	{
+		if (document.all)
+		 {
+		  document.all.sound.src = "sounds/ingles/animales/bird.mp3";
+		 }
+	}
+
+	else {
+			{
+			var audio = document.getElementById("bird"); audio.play(); 
+			audio.play();
+			}
+		}	
+}
+
+function Pig() 
+{
+	if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1))
+	{
+		if (document.all)
+		 {
+		  document.all.sound.src = "sounds/ingles/animales/pig.mp3";
+		 }
+	}
+
+	else {
+			{
+			var audio = document.getElementById("pig"); audio.play(); 
+			audio.play();
+			}
+		}	
+}
+
+function Rabbit() 
+{
+	if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1))
+	{
+		if (document.all)
+		 {
+		  document.all.sound.src = "sounds/ingles/animales/rabbit.mp3";
+		 }
+	}
+
+	else {
+			{
+			var audio = document.getElementById("rabbit"); audio.play(); 
+			audio.play();
+			}
+		}	
+}
