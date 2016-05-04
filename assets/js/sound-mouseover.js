511a94ac7ab5039d4161427c9cea67b49140f8d2
HenryNavarro <blackdemon.mx@hotmail.com>
2016-05-04 10:36:59 -0500

    rework

---
+++ assets/js/sound-mouseover.js
@@ -0,0 +1,73 @@
+
+function Cat() 
+{
+	if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1))
+	{
+		if (document.all)
+		 {
+		  document.all.sound.src = "sounds/ingles/animales/cat.mp3";
+		 }
+	}
+
+	else {
+			{
+			var audio = document.getElementById("cat"); audio.play(); 
+			audio.play();
+			}
+		}	
+}
+
+function Dog() 
+{
+	if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1))
+	{
+		if (document.all)
+		 {
+		  document.all.sound.src = "sounds/ingles/animales/dog.mp3";
+		 }
+	}
+
+	else {
+			{
+			var audio = document.getElementById("dog"); audio.play(); 
+			audio.play();
+			}
+		}	
+}
+
+function Monkey() 
+{
+	if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1))
+	{
+		if (document.all)
+		 {
+		  document.all.sound.src = "sounds/ingles/animales/dog.mp3";
+		 }
+	}
+
+	else {
+			{
+			var audio = document.getElementById("monkey"); audio.play(); 
+			audio.play();
+			}
+		}	
+}
+
+function Lion() 
+{
+	if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1))
+	{
+		if (document.all)
+		 {
+		  document.all.sound.src = "sounds/ingles/animales/dog.mp3";
+		 }
+	}
+
+	else {
+			{
+			var audio = document.getElementById("lion"); audio.play(); 
+			audio.play();
+			}
+		}	
+}
+
