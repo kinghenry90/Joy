8602bc6a0e838524b301dbc201bb6958af6dead8
HenryNavarro <blackdemon.mx@hotmail.com>
2016-05-23 21:50:10 -0500

    ejercicios

---
+++ assets/js/espanolejercicio1.js
@@ -0,0 +1,38 @@
+
+var numQues = 3;
+var numChoi = 3;
+
+var answers = new Array(5);
+answers[0] = "c";
+answers[1] = "b";
+answers[2] = "a";
+
+
+function getScore(form) {
+  var score = 0;
+  var currElt;
+  var currSelection;
+
+  for (i=0; i<numQues; i++) {
+    currElt = i*numChoi;
+    for (j=0; j<numChoi; j++) {
+      currSelection = form.elements[currElt + j];
+      if (currSelection.checked) {
+        if (currSelection.value == answers[i]) {
+          score++;
+          break;
+        }
+      }
+    }
+  }
+
+  score = Math.round(score/numQues*100);
+  form.percentage.value = score + "%";
+
+  var correctAnswers = "";
+  for (i=1; i<=numQues; i++) {
+    correctAnswers += i + ". " + answers[i-1] + "\r\n";
+  }
+  form.solutions.value = correctAnswers;
+
+}
